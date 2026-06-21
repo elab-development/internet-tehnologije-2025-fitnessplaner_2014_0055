45d93779cf86835a4380280d8a561c2a8f10a59b
Petar-Avakumovic <petarava135@gmail.com>
2026-06-22 00:41:40 +0200

    security - rate limiter

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { register, login, logout } = require('../controllers/authController');
 const auth = require('../middleware/auth');
+const authLimiter = require('../middleware/rateLimiter');
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', authLimiter, register);
+router.post('/login', authLimiter, login);
 router.post('/logout', auth, logout);
 
 module.exports = router;
